a1a47c517be016feb4c1a017e91c7c633fc0790f
zhaolonghui <anhuiwuyanzhu@gmail.com>
2022-10-14 17:00:56 +0800

    完善评论

--- src/main/java/com/lemon/violet/constant/KeyConstant.java
+++ src/main/java/com/lemon/violet/constant/KeyConstant.java
@@ -5,4 +5,8 @@ public class KeyConstant {
     public static final String SECURITY = "security";
 
     public static final String LOGIN_KEY_PREFIX = "login:";
+
+    public static final String ARTICLE_COMMENT = "0";
+
+    public static final String LINK_COMMENT = "1";
 }
